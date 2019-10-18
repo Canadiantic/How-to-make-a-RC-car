7b079a84090eba3e71bbc14e942fe12c4b9f9c9a
Canadiantic <32183437+Canadiantic@users.noreply.github.com>
2019-10-18 15:55:44 +1100

    Add files via upload

---
+++ Code/receiver.ino
@@ -0,0 +1,107 @@
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//create an RF24 object
+RF24 radio(9, 8);  // CE, CSN
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+
+const int channel_a_enable  = 6;
+const int channel_a_input_1 = 4;
+const int channel_a_input_2 = 7;
+const int channel_b_enable  = 5;
+const int channel_b_input_3 = 3;
+const int channel_b_input_4 = 2;
+
+void setup()
+{
+  while (!Serial);
+    Serial.begin(9600);
+
+  pinMode( channel_a_enable, OUTPUT );  // Channel A enable
+  pinMode( channel_a_input_1, OUTPUT ); // Channel A input 1
+  pinMode( channel_a_input_2, OUTPUT ); // Channel A input 2
+  
+  pinMode( channel_b_enable, OUTPUT );  // Channel B enable
+  pinMode( channel_b_input_3, OUTPUT ); // Channel B input 3
+  pinMode( channel_b_input_4, OUTPUT ); // Channel B input 4
+  radio.begin();
+  
+  //set the address
+  radio.openReadingPipe(0, address);
+  
+  //Set module as receiver
+  radio.startListening();
+}
+int forward=0;
+int side=0;
+void loop()
+{
+  Serial.print("OOF");
+  if (radio.available())
+  {
+    Serial.print("hello?");
+    int text[32] = {0};
+    radio.read(&text, sizeof(text));
+    //Serial.println(text[0]);
+    //Serial.println(text[1]);
+    forward=text[0];
+    side=text[1];
+
+    if(forward<0)
+    {
+      float power=-forward*1/float(255);
+      if(side<0)
+      {
+        float left=(side+255)*1/float(255);
+        analogWrite(channel_a_enable, 255*power);
+        digitalWrite( channel_a_input_1, LOW);
+        digitalWrite( channel_a_input_2, HIGH);
+
+        analogWrite(channel_b_enable, 255*power*left);
+        digitalWrite( channel_b_input_3, HIGH);
+        digitalWrite( channel_b_input_4, LOW);
+      }
+      else if(side>=0)
+      {
+        float left=-(side-255)*1/float(255);
+        analogWrite(channel_a_enable, 255*power*left);
+        digitalWrite( channel_a_input_1, LOW);
+        digitalWrite( channel_a_input_2, HIGH);
+
+        analogWrite(channel_b_enable, 255*power);
+        digitalWrite( channel_b_input_3, HIGH);
+        digitalWrite( channel_b_input_4, LOW);
+      }
+    }
+    else if(forward>=0)
+    {
+      float power=forward*1/float(255);
+      if(side<0)
+      {
+        float left=(side+255)*1/float(255);
+        analogWrite(channel_a_enable, 255*power);
+        digitalWrite( channel_a_input_1, HIGH);
+        digitalWrite( channel_a_input_2, LOW);
+
+        analogWrite(channel_b_enable, 255*power*left);
+        digitalWrite( channel_b_input_3, LOW);
+        digitalWrite( channel_b_input_4, HIGH);        
+      }
+      else if(side>=0)
+      {
+        float left=-(side-255)*1/float(255);
+        analogWrite(channel_a_enable, 255*power*left);
+        digitalWrite( channel_a_input_1, HIGH);
+        digitalWrite( channel_a_input_2, LOW);
+
+        analogWrite(channel_b_enable, 255*power);
+        digitalWrite( channel_b_input_3, LOW);
+        digitalWrite( channel_b_input_4, HIGH);        
+      }
+    }
+  }
+}
